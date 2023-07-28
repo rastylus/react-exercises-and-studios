c2c1375dc37ffbefe4b84c870f778e9526ee62cd
rastylus <ryan.p.riggs@gmail.com>
2023-07-28 10:14:51 -0500

    fix?

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,30 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
+
+const listItem = oceans.map(ocean =>
+   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <h1>{ocean.name}</h1>
+      <img src={ocean.image} alt={ocean.name} className="img"/>
+      <h2>Fun Facts</h2>
+         <ol>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+         <Button />
+
+         </ol>
+   </div>
+   );
 
 
 function Profile()
 {
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+      <ul>
+         
+         {listItem}
+      </ul>
    );
 }
 

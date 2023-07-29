8f159a25c21e9c37aec108111c94e1ff8b50d078
rastylus <ryan.p.riggs@gmail.com>
2023-07-29 14:07:36 -0500

    added html inputs

--- part3/exercises/src/components/Projects.js
+++ part3/exercises/src/components/Projects.js
@@ -5,16 +5,31 @@ import { useState } from "react"
 
 export default function MyProjects () {
     const [index, setIndex] = useState(0);
-    // let data = data.projects;
+    let samp = data.projects;
+    // let group = projects[index];
 
     return (
         <div>
-            <button onclick={handleClick}>Next</button>
-            <p>Info here{data.projects.sample}</p>
+            <button onClick={handleClick}>Next</button>
+            {/* <p>Info here{samp.sample}</p> */}
+            <br></br>
+            <br></br>
+                <h4>Original Sample</h4>
+                    <p>{`${samp[index].sample}`}</p>
+                    <img src={samp[index].samplePhoto}/>
+                <br></br>
+                <br></br>
+                <h4>Sampled By</h4>
+                    <p>{`${samp[index].flipper}`}</p>
+                    <p>{`${samp[index].song}`}</p>
+                <img src={samp[index].songPhoto}/>
+            {/* <p>{`${samp[index].sample}`}</p> */}
+            {/* {JSON.stringify(samp[index])} */}
         </div>
     );
 //     let data = projects
     function handleClick() {
+        // console.log("test")
         if (index < data.projects.length-1)
         {
            setIndex(index + 1);
@@ -26,3 +41,17 @@ export default function MyProjects () {
      }
 
 }
+
+// export default function MyProjects () {
+
+
+    //     console.log(data.projects[1]);
+      
+    //     return (
+    //       <div>
+    //         {/* {data.projects} */}
+    //         <p>{JSON.stringify(data.projects[0])}</p>
+    //       </div>
+    //     );
+      
+    //   };

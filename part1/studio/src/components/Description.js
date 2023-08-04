3223a3abe62f86c23e7d2d11ab84508fff5aeb46
rastylus <ryan.p.riggs@gmail.com>
2023-08-03 19:17:11 -0500

    created description and styling module

---
+++ part1/studio/src/components/Description.js
@@ -0,0 +1,14 @@
+function RecipeAuthor() {
+    const authorLink = "";
+    const authorPhoto = "";
+    const authorName = "";
+    return (
+        <div className = {styles.recipeAuthorBlock}>
+           <img src={authorPhoto} alt = "Reasonable alt text" className={styles.imageUpdates} />
+           <div>
+              <h3>{authorName}</h3>
+              <a href={authorLink}>Blog name</a> 
+           </div>
+        </div>
+     );
+}

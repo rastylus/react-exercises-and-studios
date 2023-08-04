38f6ca3d5a54ca21de05c74758ba0807ac3dcff1
rastylus <ryan.p.riggs@gmail.com>
2023-08-03 19:23:04 -0500

    dded ingredients and styling

--- part1/studio/src/App.js
+++ part1/studio/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import RecipeDescription from './components/Description';
+import RecipeIngredients from './components/Ingredients';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 
       <div>
           <RecipeDescription />
+          <RecipeIngredients />
       </div>
     </div>
   );

bb4502115d66c5fc26ff0595e339017d4c72a679
rastylus <ryan.p.riggs@gmail.com>
2023-08-03 19:25:32 -0500

    added recipe photo

---
+++ part1/studio/src/components/Photos.js
@@ -0,0 +1,9 @@
+import styles from './Description.module.css';
+
+function RecipePhoto() {
+    return (   
+    <img src="https://www.kingarthurbaking.com/sites/default/files/styles/kaf_thumbnail_small_2x_400x271_/public/2021-01/Pierogi%20Polands%20favorite%20comfort%20food-2.jpg?itok=1rrlkrwd" alt="recipe photo" className = {styles.imageUpdates} />
+    )
+}
+
+export default RecipePhoto;

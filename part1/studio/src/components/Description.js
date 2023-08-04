9480f2376570a70e50cb74ae38b8f4c3c3331cb9
rastylus <ryan.p.riggs@gmail.com>
2023-08-03 19:20:37 -0500

    added recipe description to app.js

--- part1/studio/src/components/Description.js
+++ part1/studio/src/components/Description.js
@@ -1,14 +1,35 @@
+import styles from './Description.module.css';
+import React from 'react';
+
+
 function RecipeAuthor() {
-    const authorLink = "";
-    const authorPhoto = "";
-    const authorName = "";
+    const authorLink = "https://www.kingarthurbaking.com/sites/default/files/styles/featured_image_2x/public/recipe_legacy/1750-3-large.jpg?itok=MwEaAU-G";
+    const authorPhoto = "https://www.kingarthurbaking.com/sites/default/files/styles/scaled_very_small_250w/public/2019-07/mary-jane-robbins-blog-author.jpg?itok=wkG8pQfZ";
+    const authorName = "MaryJane Robbins";
     return (
         <div className = {styles.recipeAuthorBlock}>
-           <img src={authorPhoto} alt = "Reasonable alt text" className={styles.imageUpdates} />
+           <img src={authorPhoto} alt = "MaryJane Robbins" className={styles.imageUpdates} />
            <div>
               <h3>{authorName}</h3>
-              <a href={authorLink}>Blog name</a> 
+              <a href={authorLink}>Pierogie Recipe</a> 
            </div>
         </div>
      );
 }
+
+class RecipeDescription extends React.Component {
+    render() {
+        return (
+
+            <div> 
+            <div>
+                <h1>Recipe Title</h1>
+                <p>Short recipe description</p>
+            </div>
+            <RecipeAuthor />
+        </div>
+        )
+    }
+ }
+
+ export default RecipeDescription;

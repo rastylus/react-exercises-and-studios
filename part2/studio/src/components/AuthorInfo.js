aa1204e4c9e7ba1b04b2cd88436cfec9cf288a44
rastylus <ryan.p.riggs@gmail.com>
2023-08-07 19:54:59 -0500

    added author info

--- part2/studio/src/components/AuthorInfo.js
+++ part2/studio/src/components/AuthorInfo.js
@@ -1,5 +1,26 @@
+import "./styling.css"
+import recipedata from "./recipe.json";
+
 function AuthorInfo() {
-   return;
+  const recipeAuthor = recipedata.map((data) =>
+  <div key={data.name}>{data.author}</div>
+  )
+  const recipeAuthorImage = recipedata.map((data) =>
+  <div key={data.name}>
+      <img src={data.authorImage} alt={data.author} className="authorImage" />
+  </div>
+  )
+  const recipeWebsite = recipedata.map((data) =>
+    <a key={data.name}>{data.website}</a>
+  )
+   return (
+    <div>
+      {recipeAuthorImage}
+      {recipeAuthor}
+      {recipeWebsite}
+    </div>
+   );
+  // console.log(recipeAuthor)
  }
  
  export default AuthorInfo;
